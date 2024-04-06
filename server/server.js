811ef248229378eb4b6f3d4015d3c827f0f0973c
Brody <brodyover@gmail.com>
2024-04-06 17:15:02 -0400

    proper structure

--- server/server.js
+++ server/server.js
@@ -44,14 +44,6 @@ app.post('/api', async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, './dist')));
-
-// Catch-all route to serve the Vite React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist', 'index.html'));
-});
-
-
 app.listen(PORT, () => {
   console.log(`\x1b[31m                                                                      
  _____ _____ _____ _____ _____    __    _____ _____ _____ __    __    
