2ca48ab5543dc9a0f5707b779552c9a30c989131
Emanuel Rajic <90189726+Vjecni@users.noreply.github.com>
2024-04-09 16:53:26 +0200

    Add files via upload

--- setupProxy.js
+++ setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api/',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:3000',  // Assuming your server is running on port 3000
+      target: process.env.REACT_APP_API_URL,  // Assuming your server is running on port 3000
       changeOrigin: true,
     })
   );
