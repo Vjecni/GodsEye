d0306a3434430581a2c055e067df74a337181e8c
Emanuel Rajic <90189726+Vjecni@users.noreply.github.com>
2024-04-09 13:21:06 +0200

    Add files via upload

---
+++ setupProxy.js
@@ -0,0 +1,12 @@
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api/',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:3000',  // Assuming your server is running on port 3000
+      changeOrigin: true,
+    })
+  );
+};
