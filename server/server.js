d2055c4bbf58e23810cdf4d2ffc64802434b7528
Emanuel Rajic <90189726+Vjecni@users.noreply.github.com>
2024-07-23 22:54:46 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-  console.log(`Received request: ${req.url}`);
+  console.log(`Request Received\nIP: ${req.ip}\nRequest URL: ${req.url}`);
   next();
 });
 
